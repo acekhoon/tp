37a047c78b74bda67efc9978d8380dbd15116b01
Ikhoon <e0908299@u.nus.edu>
2024-03-15 01:14:58 +0800

    Tutorial Done

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,36 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+public class Remark {
+    public final String value;
+
+    public Remark(String remark) {
+        requireNonNull(remark);
+        value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Remark)) {
+            return false;
+        }
+
+        return this.value.equals(((Remark) other).value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
